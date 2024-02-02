667e52d8831e126a6426cf098cea643d33c1f83e
Nitesh Khatri <nitesh@usedatabrain.com>
2024-02-02 11:21:30 +0530

    eslint and prettier setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "standard-with-typescript",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  ignorePatterns: ["node_modules", "dist", ".eslintrc.cjs", "vite.config.ts"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "import/extensions": "off",
+    "prettier/prettier": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+    "array-element-newline": [
+      "error",
+      {
+        ArrayExpression: "consistent",
+        ArrayPattern: { multiline: true, minItems: 3 },
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          ["internal", "parent", "sibling", "index"],
+        ],
+      },
+    ],
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
     }),
+    tsconfigPaths(),
   ],
   build: {
     lib: {
